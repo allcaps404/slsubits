a8b6adb4adcccada1f4630f43a43672dd5a40821
rexal <rextoldz26@gmail.com>
2025-02-05 14:10:14 +0800

    added middleware to admin and student

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\IfAdmin::class,
+        'student' => \App\Http\Middleware\Ifstudent::class,
     ];
     protected $routeMiddleware = [
         'role' => \App\Http\Middleware\RoleMiddleware::class,
