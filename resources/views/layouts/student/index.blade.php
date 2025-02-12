0b7b0e8dc38cb99ca6067214e90369f1f13f35fe
rexal <rextoldz26@gmail.com>
2025-02-12 10:40:24 +0800

    mod

--- resources/views/layouts/student/index.blade.php
+++ resources/views/layouts/student/index.blade.php
@@ -2,6 +2,7 @@
     <head>
         <meta charset="utf-8" />
         <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title> User Account Landing Page </title>
         <script src="https://cdn.tailwindcss.com"></script>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
