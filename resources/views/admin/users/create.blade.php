fd380818c62dcc82af674f5d2477789ce5022c1b
rexal <rextoldz26@gmail.com>
2025-03-18 18:27:15 +0800

    mod

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -207,5 +207,22 @@
             preview.src = "https://www.gravatar.com/avatar/?d=mp";
         }
     }
+   
+    document.getElementById('toggleButton').addEventListener('click', function () {
+        const passwordField = document.getElementById('password');
+        const toggleIcon = document.getElementById('toggleIcon');
+
+        if (passwordField.type === 'password') {
+            passwordField.type = 'text';
+            toggleIcon.classList.remove('fa-eye');
+            toggleIcon.classList.add('fa-eye-slash');
+        } else {
+            passwordField.type = 'password';
+            toggleIcon.classList.remove('fa-eye-slash');
+            toggleIcon.classList.add('fa-eye');
+        }
+    });
+</script>
+
 </script>
 @endsection
